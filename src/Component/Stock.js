0473fc0c3ff2eeb9a7f75a96603dc4755f2fa4d8
Proramaporn <64032743+Proramaporn@users.noreply.github.com>
2021-02-27 01:19:47 +0700

    stock

--- src/Component/Stock.js
+++ src/Component/Stock.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import history from '../history'
 import Topbar from './Topbar'
+import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBCol, MDBRow } from 'mdbreact';
 
 class Dashboard extends Component {
   constructor(props) {
@@ -12,20 +13,41 @@ class Dashboard extends Component {
   render() {
     return (
       <div className="bg">
-        <Topbar/>
-        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-          <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-            <h1>Stock</h1>
-          </div>
-          <button onClick={() => history.push('/stock/viewStock')}>
-            View Stock
-            </button>
-          <button onClick={() => history.push('/stock/countingStock')}>
-            Counting Stock
-            </button>
-          <button onClick={() => history.push('/stock/editStock')}>
-            Edit Stock
-            </button>
+        <Topbar />
+
+
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center',paddingTop:"11%" }}>
+          <MDBRow>
+            <MDBCard style={{width: "18rem" ,margin:"1rem"}} onClick={() => history.push('/stock/viewStock')}>
+              <MDBCardImage
+                className="img-fluid"
+                src="https://uppic.cc/d/hQKChem6SpIiFFxAn439Y"/>
+                <div class="d-flex justify-content-center">
+                  <MDBCardTitle >View Stock</MDBCardTitle>
+                </div> 
+            </MDBCard>
+
+            <MDBCard style={{ width: "18rem" ,margin:"1rem"}} onClick={() => history.push('/stock/countingStock')}>
+              <MDBCardImage
+                style={{width: "100%",resizeMode: 'contain'}}
+                className="img-fluid"
+                src="https://uppic.cc/d/9vOtr_EuhpJRMurA4UXvp"/>
+              <div class="d-flex justify-content-center">
+                <MDBCardTitle>Counting Stock</MDBCardTitle>
+              </div> 
+              
+            </MDBCard>
+
+            <MDBCard style={{ width: "18rem",margin:"1rem" }} onClick={() => history.push('/stock/editStock')}>
+              <MDBCardImage
+                className="img-fluid"
+                src="https://uppic.cc/d/0W3RIXMsbwLZibPPoOvcF"/>
+              <div class="d-flex justify-content-center">
+                <MDBCardTitle>Edit Stock</MDBCardTitle>
+              </div> 
+              
+            </MDBCard>
+          </MDBRow>
         </div>
       </div>
 

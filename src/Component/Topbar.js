e24c53a3a9068d2f44a77ebc75253c08003e0a37
Mew Prapassorn <78916209+Prapassorn6848@users.noreply.github.com>
2021-02-27 01:25:31 +0700

    back to the home

--- src/Component/Topbar.js
+++ src/Component/Topbar.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Style.css'
 import { Nav, Navbar, Button, Form, FormControl, NavDropdown } from 'react-bootstrap'
-import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBCollapse, MDBContainer,
-    MDBHamburgerToggler } from 'mdbreact';
+import {
+    MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBCollapse, MDBContainer,
+    MDBHamburgerToggler
+} from 'mdbreact';
+import history from '../history'
 
 class Topbar extends Component {
     constructor(props) {
@@ -13,18 +16,18 @@ class Topbar extends Component {
 
     render() {
         return (
-                <div class="topbar">
-                    <Navbar >
-                    <MDBHamburgerToggler color="#FFFFFF" id="hamburger1"   />
-                        <Navbar href="/" style={{color: 'white',height:40,fontSize:20}}>
+            <div class="topbar">
+                <Navbar >
+                    <MDBHamburgerToggler color="#FFFFFF" id="hamburger1" />
+                    <Navbar href="/" style={{ color: 'white', height: 40, fontSize: 20 }} onClick={() =>history.push('/home')}  >
                         <img
-                                src="https://scontent.fbkk11-1.fna.fbcdn.net/v/t1.15752-9/152237023_129232515675141_1331579132455301323_n.png?_nc_cat=107&ccb=3&_nc_sid=ae9488&_nc_ohc=6pHEX8RMxnUAX9IurH4&_nc_ht=scontent.fbkk11-1.fna&oh=97ee3e7acba83e5b06fbb3574d54aa68&oe=605471E7"
-                                height="50"
-                                alt=""
-                                loading="lazy"
+                            src="https://scontent.fbkk11-1.fna.fbcdn.net/v/t1.15752-9/152237023_129232515675141_1331579132455301323_n.png?_nc_cat=107&ccb=3&_nc_sid=ae9488&_nc_ohc=6pHEX8RMxnUAX9IurH4&_nc_ht=scontent.fbkk11-1.fna&oh=97ee3e7acba83e5b06fbb3574d54aa68&oe=605471E7"
+                            height="50"
+                            alt=""
+                            loading="lazy"
                             />KLUNG CHANA</Navbar>
-                    </Navbar>
-                </div>
+                </Navbar>
+            </div>
         )
     }
 }

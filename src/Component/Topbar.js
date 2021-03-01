1df5359c6ae044bffa28eff88c49d4b077b17e15
Pkinnz <ratchapol.at@ku.th>
2021-03-01 22:54:23 +0700

    Update Topbar.js

--- src/Component/Topbar.js
+++ src/Component/Topbar.js
@@ -29,7 +29,6 @@ class Topbar extends Component {
         return (
             <div class="topbar">
                 <Navbar >
-                    <MDBHamburgerToggler color="#FFFFFF" id="hamburger1" />
                     <Navbar href="/" style={{ color: 'white', height: 40, fontSize: 20 }} onClick={() => history.push('/home')}  >
                         <img
                             src="https://scontent.fbkk11-1.fna.fbcdn.net/v/t1.15752-9/152237023_129232515675141_1331579132455301323_n.png?_nc_cat=107&ccb=3&_nc_sid=ae9488&_nc_ohc=6pHEX8RMxnUAX9IurH4&_nc_ht=scontent.fbkk11-1.fna&oh=97ee3e7acba83e5b06fbb3574d54aa68&oe=605471E7"
@@ -42,7 +41,6 @@ class Topbar extends Component {
                     <a1>{this.state.name}</a1>
                 </Navbar>
             </div>
-            </div >
         )
     }
 }

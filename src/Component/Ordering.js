97709e9ea949ecb7862c718dbf4415c910c05c0d
Pkinnz <ratchapol.at@ku.th>
2021-03-04 00:46:31 +0700

    Ordering

--- src/Component/Ordering.js
+++ src/Component/Ordering.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import history from '../history'
 import Topbar from './Topbar'
 import Hamburger from './Hamburger'
+import Paper from '@material-ui/core/Paper';
+import './Style.css'
 
 import { connect } from 'react-redux';
 
@@ -16,35 +18,45 @@ class Ordering extends Component {
     render() {
         return (
             <div className="bg">
-                <Hamburger page='ORDERING' user={this.state.user} />
-                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'Right' }}>
-                    <div>
-                        <div>
-                            <p>Supplier</p>
-                            <input type="text" />
+                <Paper className="paperOrdering">
+                    <div style={{ display: 'flex', flexDirection: 'row' }}>
+                        <div style={{ paddingTop: 110, paddingLeft: 400 }}>
+                            <p className="textOrSup">Supplier</p>
+                            <input type="text" style={{ fontSize: 24, }}></input>
                         </div>
-                        <div>
-                            <p>Address</p>
-                            <input type="text" />
-                        </div>
-                        <div>
-                            <p>Tel.</p>
-                            <input type="text" />
+                        <div style={{ paddingTop: 110, paddingLeft: 600 }}>
+                            <p className="textOrDate">Date</p>
+                            <input type="text" style={{ fontSize: 24, }} />
                         </div>
                     </div>
+                    <div style={{ display: 'flex', flexDirection: 'row' }}>
+                        <div style={{ paddingTop: 40, paddingLeft: 400 }}>
+                            <p className="textOrAddr">Address</p>
+                            <textarea type="text" style={{ fontSize: 24, paddingBottom: 110 }}></textarea>
+                        </div>
+                        <div style={{ display: 'flex', flexDirection: 'column' }} >
+                            <div style={{ paddingTop: 40, paddingLeft: 615 }}>
+                                <p className="textOrConN">Contact Name</p>
+                                <input type="text" style={{ fontSize: 24, }} />
+                            </div>
+                            <div style={{ paddingTop: 40, paddingLeft: 615 }}>
+                                <p className="textOrTelAd">Tel.</p>
+                                <input type="text" style={{ fontSize: 24, }} />
+                            </div>
+                        </div>
+
+                    </div>
+                    <div style={{ display: 'flex', flexDirection: 'row' }}>
+                        <div style={{ paddingTop: 40, paddingLeft: 400 }}>
+                            <p className="textOrTelCn">Tel.</p>
+                            <input type="text" style={{ fontSize: 24, }}></input>
+                        </div>
+
+                    </div>
                     <div>
-                        <div>
-                            <p>Date</p>
-                            <input type="text" />
-                        </div>
-                        <div>
-                            <p>Contact Name</p>
-                            <input type="text" />
-                        </div>
-                        <div>
-                            <p>Tel.</p>
-                            <input type="text" />
-                        </div>
+
+
+
                         <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
                             <div>
                                 <button onClick={() => history.push('/home')}>
@@ -57,7 +69,10 @@ class Ordering extends Component {
                         </div>
 
                     </div>
-                </div>
+
+                </Paper>
+                <Hamburger page='ORDERING' user={this.state.user} />
+
             </div>
 
         )

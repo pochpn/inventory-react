bbdf67216adb7b26577eea99fe8fcae50249c744
Pkinnz <ratchapol.at@ku.th>
2021-03-04 22:09:25 +0700

    Temp Orderingg

---
+++ src/Component/OrderingChart.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import history from '../history'
+import Topbar from './Topbar'
+import Hamburger from './Hamburger'
+import Paper from '@material-ui/core/Paper';
+import './Style.css'
+
+import styled, { css } from 'styled-components'
+import { connect } from 'react-redux';
+
+
+
+class Ordering extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: this.props.userList[this.props.userList.length - 1],
+        };
+    }
+
+    render() {
+        return (
+            <div className="bg">
+                <Paper className ='schOrder'>
+
+                </Paper>
+                <Paper className ='topOrdering'>
+
+                </Paper>
+                <Paper className ='topicLeft'>
+
+                </Paper>
+                <Paper className ='topicRight'>
+
+                </Paper>
+                <Paper className ='dataLeft'>
+
+                </Paper>
+                <Paper className ='dataRight'>
+
+                </Paper>
+                <Hamburger page='ORDERING' user={this.state.user} />
+
+            </div>
+
+        )
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+
+    };
+};
+
+const mapStateToProps = (state) => {
+    return {
+        userList: state.userReducer.userList,
+        accountList: state.accountReducer.accountList
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Ordering);

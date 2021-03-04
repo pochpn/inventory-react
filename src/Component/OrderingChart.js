27ddbb992e462a1a3b08e4cdbf29edea8edfc8c5
Pkinnz <ratchapol.at@ku.th>
2021-03-05 00:28:03 +0700

    OrderingChart

--- src/Component/OrderingChart.js
+++ src/Component/OrderingChart.js
@@ -4,11 +4,38 @@ import Topbar from './Topbar'
 import Hamburger from './Hamburger'
 import Paper from '@material-ui/core/Paper';
 import './Style.css'
+import { cost, dashboard, historyPic, orderconfirm, ordering, packing, standcount, confirmship, xxx, search } from '../pic'
 
 import styled, { css } from 'styled-components'
 import { connect } from 'react-redux';
 
+const ButtonCancel = styled.button`
+  background: #A09797;
+  border-radius: 10px;
+  border: 2px;
+  color: #ffffff;
+`
 
+const ButtonClear = styled.button`
+  background: #A09797;
+  border-radius: 10px;
+  border: 2px;
+  color: #ffffff;
+`
+
+const ButtonNext = styled.button`
+  background: #EF3F3E;
+  border-radius: 10px;
+  border: 2px;
+  color: #000000;
+`
+
+const ButtonAdd = styled.button`
+  background: #EF3F3E;
+  border-radius: 100px;
+  border: 2px;
+  color: #ffffff;
+`
 
 class Ordering extends Component {
     constructor(props) {
@@ -21,23 +48,80 @@ class Ordering extends Component {
     render() {
         return (
             <div className="bg">
-                <Paper className ='schOrder'>
+                <Paper className='schOrder'>
+                    <div style={{ display: 'flex', flexDirection: 'row' }}>
+                        <div style={{ paddingTop: 70, paddingLeft: 30 }}>
+                            <input type="text" style={{ fontSize: 13 }}></input>
+                        </div>
+                        <div style={{ paddingTop: 70, paddingLeft: 68 }}>
+                            <input type="text" style={{ fontSize: 13 }}></input>
+                        </div>
+                        <div>
+                            <img img className="imsch" src={search} />
+                        </div>
+                    </div>
+                    <div style={{ display: 'flex', flexDirection: 'row' }}>
+                        <div >
+                            <p className="txtProID">Product ID</p>
+                        </div>
+                        <div>
+                            <p className="txtProOr">Or</p>
+                        </div>
+                        <div>
+                            <p className="txtProNm">Product Name</p>
+                        </div>
+
+                    </div>
 
                 </Paper>
-                <Paper className ='topOrdering'>
+                <Paper className='topOrdering'>
+                    <p className="txtTop">Ordering Product</p>
+                </Paper>
+                <Paper className='topicLeft'>
+                    <p className="txtTopL1">Product</p>
+                    <p className="txtTopL2">Product ID</p>
+                    <p className="txtTopL3">Product Name</p>
+                </Paper>
+                <Paper className='topicRight'>
+                    <p className="txtTopR1">Product</p>
+                    <p className="txtTopR2">Exp.</p>
+                    <p className="txtTopR3">Shelf</p>
+                    <p className="txtTopR4">Level</p>
+                    <p className="txtTopR5">Cost/Unit</p>
+                    <p className="txtTopR6">QTY</p>
+                    <p className="txtTopR7">Amount</p>
 
                 </Paper>
-                <Paper className ='topicLeft'>
+                <Paper className='dataLeft'>
 
                 </Paper>
-                <Paper className ='topicRight'>
+                <Paper className='dataRight'>
 
                 </Paper>
-                <Paper className ='dataLeft'>
+                <Paper className="buttonOrder">
+                    <div style={{ display: 'flex', flexDirection: 'row' }}>
+                        <div style={{ paddingLeft: 50, paddingTop: 10}}>
+                            <ButtonAdd style={{ fontSize: 35, width: 70, height: 70 }} >
+                                +
+                            </ButtonAdd>
+                        </div>
+                        <div style={{ paddingLeft: 60, paddingTop: 35 }}>
+                            <ButtonCancel style={{ fontSize: 25, width: 184, height: 52 }} >
+                                Cancel
+                            </ButtonCancel>
+                        </div>
+                        <div style={{ paddingLeft: 690, paddingTop: 35 }}>
+                            <ButtonClear style={{ fontSize: 25, width: 184, height: 52 }} >
+                                Clear
+                            </ButtonClear>
+                        </div>
+                        <div style={{ paddingLeft: 10, paddingTop: 35 }}>
+                            <ButtonNext style={{ fontSize: 25, width: 184, height: 52 }}>
+                                Next
+                            </ButtonNext>
+                        </div>
 
-                </Paper>
-                <Paper className ='dataRight'>
-
+                    </div>
                 </Paper>
                 <Hamburger page='ORDERING' user={this.state.user} />
 

9d9d256701e64adbefe69c4bc6361110f6916580
pochpn <pochpn@gmail.com>
2021-03-10 03:30:02 +0700

    Add Product

--- src/Component/AddProduct.js
+++ src/Component/AddProduct.js
@@ -19,18 +19,15 @@ class AddProduct extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            employeeID: null,
-            department: 'Select Department',
-            departmentID: null,
-            firstnameTH: null,
-            lastnameTH: null,
-            firstnameEN: null,
-            lastnameEN: null,
-            idCard: null,
-            birthDate: null,
-            tel: null,
-            email: null,
-            address: null,
+            productID: null,
+            type: null,
+            productName: null,
+            price: null,
+            unit: null,
+            date: null,
+            shelf: null,
+            level: null,
+            product: this.props.productList[this.props.productList.length - 1],
             user: this.props.userList[this.props.userList.length - 1],
             pic: null,
         };
@@ -56,24 +53,19 @@ class AddProduct extends Component {
     uploadSuccess = (uri) => {
         console.log(uri)
         this.setState({ pic: uri })
-        const user = {
-            employeeID: this.state.employeeID,
-            department: this.state.department,
-            departmentID: this.state.departmentID,
-            firstnameTH: this.state.firstnameTH,
-            lastnameTH: this.state.lastnameTH,
-            firstnameEN: this.state.firstnameEN,
-            lastnameEN: this.state.lastnameEN,
-            idCard: this.state.idCard,
-            birthDate: this.state.birthDate,
-            tel: this.state.tel,
-            email: this.state.email,
-            address: this.state.address,
-            pass: Base64.encode(this.state.email),
+        const product = {
+            productID: this.state.productID,
+            type: this.state.type,
+            productName: this.state.productName,
+            price: this.state.price,
+            unit: this.state.unit,
+            date: this.state.date,
+            shelf: this.state.shelf,
+            level: this.state.shelf,
             pic: uri
         }
-        firestore.addUser(user, this.addSuccess, this.addReject)
-        this.props.addAccount(user)
+        firestore.addProduct(product, this.addSuccess, this.addReject)
+        this.props.addProduct(product)
 
     }
 
@@ -106,6 +98,24 @@ class AddProduct extends Component {
                             <input type="file" onChange={this.onImageChange} style={{ width: '105px', alignSelf: 'center' }} />
                         </div>
                     </Paper>
+                    <p className="fontAddPD" style={{ top: '20%' }}>Product ID</p>
+                    <input className="inputPD1" style={{ top: '21%' }} onChange={txt => this.setState({ productID: txt.target.value })}></input>
+                    <p className="fontAddPD" style={{ top: '30%' }}>Type</p>
+                    <input className="inputPD1" style={{ top: '31%' }} onChange={txt => this.setState({ type: txt.target.value })}></input>
+                    <p className="fontAddPD" style={{ top: '40%' }}>Product Name</p>
+                    <input className="inputPD1" style={{ top: '41%' }} onChange={txt => this.setState({ productName: txt.target.value })}></input>
+                    <p className="fontAddPD" style={{ top: '50%' }}>Price/Unit</p>
+                    <input className="inputPD2" style={{ top: '51%', left: '60%' }} onChange={txt => this.setState({ price: txt.target.value })}></input>
+                    <p className="fontAddPD" style={{ top: '50%', left: '75.9%' }}>/</p>
+                    <input className="inputPD2" style={{ top: '51%', left: '78.47%' }} onChange={txt => this.setState({ unit: txt.target.value })}></input>
+                    <p className="fontAddPD" style={{ top: '60%' }}>Date</p>
+                    <input className="inputPD1" style={{ top: '61%' }} onChange={txt => this.setState({ date: txt.target.value })}></input>
+                    <p className="fontAddPD" style={{ top: '70%' }}>Shelf Location</p>
+                    <input className="inputPD3" style={{ top: '71%', left: '60%'}} onChange={txt => this.setState({ shelf: txt.target.value })}></input>
+                    <p className="fontAddPD" style={{ top: '70%',left:'74%',width:'96px' }}>Level</p>
+                    <input className="inputPD3" style={{ top: '71%', left: '82%'}} onChange={txt => this.setState({ level: txt.target.value })}></input>
+                    <button className="btCcAnp" onClick={() => history.push('/ordering/orderingChart')}>Cancel</button>
+                    <button className="btAddAnp" onClick={this.onAdd}>Add</button>
                 </Paper>
 
                 <Hamburger page='ADD NEW PRODUCT' user={this.state.user} />

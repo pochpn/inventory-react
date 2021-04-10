f0bf7153ab8aec10e01313557855b04ad8b7d683
Proramaporn <64032743+Proramaporn@users.noreply.github.com>
2021-04-10 21:16:33 +0700

    เเก้ picking

--- src/Component/ConReturned.js
+++ src/Component/ConReturned.js
@@ -30,7 +30,48 @@ class ConReturned extends Component {
     this.state = {
       date: new Date(),
       user: this.props.userList[this.props.userList.length - 1],
+      reName: '',
+      tel: '',
+      des: '',
     };
+  }
+
+  onNext = () => {
+    if ((this.state.reName !== '') && (this.state.tel !== '') && (this.state.des !== '')) {
+      let date = this.state.date
+      let year = date.getFullYear().toString().substr(2, 3)
+      let mount = date.getMonth().toString()
+      if (date.getMonth().toString().length === 1) {
+        mount = '0' + (date.getMonth() + 1).toString()
+      }
+      let day = date.getDate().toString()
+      if (date.getDate().toString().length === 1) {
+        day = '0' + date.getDate().toString()
+      }
+      let hour = date.getHours().toString()
+      if (date.getHours().toString().length === 1) {
+        hour = '0' + date.getHours().toString()
+      }
+      let min = date.getMinutes().toString()
+      if (date.getMinutes().toString().length === 1) {
+        min = '0' + date.getMinutes().toString()
+      }
+      let sec = date.getSeconds().toString()
+      if (date.getSeconds().toString().length === 1) {
+        sec = '0' + date.getSeconds().toString()
+      }
+      const info = {
+        reName: this.state.reName,
+        tel: this.state.tel,
+        des: this.state.des,
+        date: (this.state.date.getDate() + '/' + (this.state.date.getMonth() + 1) + '/' + this.state.date.getFullYear()).toString(),
+        reNum: 'RE' + year + mount + day + hour + min + sec,
+      }
+      history.push({
+        pathname: '/orderConfirm/returned/returnedChart',
+        state: { info: info },
+      })
+    }
   }
 
   render() {
@@ -45,17 +86,17 @@ class ConReturned extends Component {
             </div>
             <div style={{ display: 'flex', flexDirection: 'row', paddingTop: '2%', paddingLeft: '34%' }}>
               <p className='textOr' style={{ paddingRight: '3%', paddingTop: '1%' }}>Returned Name</p>
-              <input type="text" style={{ fontSize: 24, width: 351}} />
+              <input type="text" style={{ fontSize: 24, width: 351 }} value={this.state.reName} onChange={txt => this.setState({ reName: txt.target.value })} />
             </div>
             <div style={{ display: 'flex', flexDirection: 'row', paddingTop: '2%', paddingLeft: '34%' }}>
               <p className='textOr' style={{ paddingRight: '3%', paddingTop: '1%' }}>Tel.</p>
-              <input type="text" style={{ fontSize: 24, width: 533 }} />
+              <input type="text" style={{ fontSize: 24, width: 533 }} value={this.state.tel} onChange={txt => this.setState({ tel: txt.target.value })} />
             </div>
             <div style={{ display: 'flex', flexDirection: 'flex-start', paddingTop: '2%', paddingLeft: '34%' }}>
               <p className='textOr' >Description</p>
             </div>
             <div style={{ display: 'flex', flexDirection: 'flex-start', paddingLeft: '34%' }}>
-              <textarea type="text" max="10" style={{ fontSize: 24, paddingBottom: 100, width: 615, }} />
+              <textarea type="text" max="10" style={{ fontSize: 24, paddingBottom: 100, width: 615, }} value={this.state.des} onChange={txt => this.setState({ des: txt.target.value })} />
             </div>
 
 
@@ -68,7 +109,7 @@ class ConReturned extends Component {
             </ButtonCancel>
               </div>
               <div style={{ paddingLeft: 10, paddingTop: 160 }}>
-                <ButtonNext style={{ fontSize: 25, width: 184, height: 52 }} onClick={() => history.push('/orderConfirm/returned/returnedChart')}>
+                <ButtonNext style={{ fontSize: 25, width: 184, height: 52 }} onClick={this.onNext}>
                   Next
             </ButtonNext>
               </div>
